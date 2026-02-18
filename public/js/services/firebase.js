d64f7707f752cf6e0895123b6cd2be2bc430464a
thrillbuildercry-del <thrillbuildercry@gmail.com>
2026-02-18 05:45:45 -0600

    Update firebase.js

--- public/js/services/firebase.js
+++ public/js/services/firebase.js
@@ -18,6 +18,7 @@ import {
     where,
     orderBy, 
     onSnapshot,
+    getDocs,
     runTransaction,
     serverTimestamp 
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
@@ -55,6 +56,7 @@ export {
     where,
     orderBy,
     onSnapshot,
+    getDocs,
     runTransaction,
     serverTimestamp
 };
